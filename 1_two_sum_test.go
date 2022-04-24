6dcd35b1a3a8b8b491d9994bc81eaf20e8dc57f8
pinke <pinke.yu7@gmail.com>
2022-04-24 16:25:06 +0800

    Feature: 1. two sum

--- 1_two_sum_test.go
+++ 1_two_sum_test.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTwoSum(t *testing.T) {
-	nums := []int{2, 7, 11, 15}
-	target := 9
+	testCases := []struct {
+		nums   []int
+		target int
+		want   []int
+	}{
+		{
+			[]int{2, 7, 11, 15},
+			9,
+			[]int{0, 1},
+		},
+		{
+			[]int{3, 2, 4},
+			6,
+			[]int{1, 2},
+		},
+		{
+			[]int{3, 3},
+			6,
+			[]int{0, 1},
+		},
+	}
 
-	output := twoSum(nums, target)
-	assert.Equal(t, output, []int{0, 1})
+	for i, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("Test Case #%d,Input:nums:%v,target:%d,", i+1, tc.nums, tc.target), func(t *testing.T) {
+			output := twoSum(tc.nums, tc.target)
+			assert.Equal(t, output, tc.want)
+		})
+	}
 }
