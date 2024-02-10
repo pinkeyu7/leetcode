bfbf72458a29da6bfe504e8ed70a9d81f8bbf3eb
pinke <pinke.yu7@gmail.com>
2024-02-10 13:16:18 +0800

    Feature: 22.change recursive function name

--- 22_generate_parentheses.go
+++ 22_generate_parentheses.go
@@ -2,19 +2,19 @@ package main
 
 func generateParenthesis(n int) []string {
 	result := make([]string, 0)
-	recursive(&result, n, 0, 0, "")
+	generateParenthesisRecursive(&result, n, 0, 0, "")
 	return result
 }
 
-func recursive(result *[]string, target int, left int, right int, current string) {
+func generateParenthesisRecursive(result *[]string, target int, left int, right int, current string) {
 	if len(current) == target*2 {
 		*result = append(*result, current)
 	}
 
 	if left < target {
-		recursive(result, target, left+1, right, current+"(")
+		generateParenthesisRecursive(result, target, left+1, right, current+"(")
 	}
 	if right < left {
-		recursive(result, target, left, right+1, current+")")
+		generateParenthesisRecursive(result, target, left, right+1, current+")")
 	}
 }
