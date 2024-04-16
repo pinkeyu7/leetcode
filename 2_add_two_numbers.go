8392910c8a4fd0ee8e98ec2225c394155434081c
Pinke <pinke.yu7@gmail.com>
2024-04-16 17:03:07 +0800

    Feature: 2. Add Two Numbers

--- 2_add_two_numbers.go
+++ 2_add_two_numbers.go
@@ -1,61 +1,60 @@
 package main
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var start *ListNode
-	var current *ListNode
-
-	addFlag := false
-	for ok := true; ok; ok = (l1 != nil || l2 != nil) {
-		tempSum := 0
-		if addFlag {
-			tempSum += 1
-			addFlag = false
-		}
-
-		if l1 != nil {
-			tempSum += l1.Val
-			l1 = l1.Next
-		}
-
-		if l2 != nil {
-			tempSum += l2.Val
-			l2 = l2.Next
-		}
-
-		if tempSum >= 10 {
-			addFlag = true
-			tempSum = tempSum % 10
-		}
-
-		temp := ListNode{
-			Val:  tempSum,
-			Next: nil,
-		}
-
-		if start == nil {
-			start = &temp
-			current = start
-		} else {
-			current.Next = &temp
-			current = &temp
-		}
-
+	if l1 == nil {
+		return l2
 	}
 
-	if addFlag {
-		temp := ListNode{
-			Val:  1,
+	if l2 == nil {
+		return l1
+	}
+
+	var start *ListNode
+	var resultCursor *ListNode
+	cursor1 := l1
+	cursor2 := l2
+	moreThanTen := false
+
+	for cursor1 != nil || cursor2 != nil || moreThanTen {
+		tempSum := 0
+		if moreThanTen {
+			tempSum += 1
+		}
+
+		if cursor1 != nil {
+			tempSum += cursor1.Val
+		}
+		if cursor2 != nil {
+			tempSum += cursor2.Val
+		}
+
+		// handle bigger than ten
+		moreThanTen = tempSum/10 == 1
+
+		newNode := &ListNode{
+			Val:  tempSum % 10,
 			Next: nil,
 		}
 
-		if start == nil {
-			start = &temp
-			current = start
+		// Handle current cursor
+		if resultCursor == nil {
+			resultCursor = newNode
 		} else {
-			current.Next = &temp
-			current = &temp
+			resultCursor.Next = newNode
+			resultCursor = resultCursor.Next
 		}
 
+		// Setting start node
+		if start == nil {
+			start = newNode
+		}
+
+		if cursor1 != nil {
+			cursor1 = cursor1.Next
+		}
+		if cursor2 != nil {
+			cursor2 = cursor2.Next
+		}
 	}
 
 	return start
